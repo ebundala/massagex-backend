1398282144df8e33611fed4a892574c9f798f7f6
Elias Bundala <ebundala@gmail.com>
2022-08-07 12:47:21 +0300

    feat(serviceAc): Use service ac to auth admin fn calls

--- massagex-firebase/functions/src/index.ts
+++ massagex-firebase/functions/src/index.ts
@@ -2,20 +2,31 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import axios from "axios";
-
-
+const APP_SERVER_URL:string = process.env.APP_SERVER_URL as string;
+const ADMIN_UID:string = process.env.ADMIN_UID as string;
+const SERVICE_AC_ID:string = process.env.SERVICE_AC_ID as string;
+admin.initializeApp({
+  serviceAccountId: SERVICE_AC_ID,
+}
+);
 type NotifyUserCreateResult = {data:{
   notifyUserSignup:Record<string, unknown>
 }};
 type UserRoles = {data:{
   getUserClaims:Record<string, unknown>
 }};
-const APP_SERVER_URL:string = process.env.APP_SERVER_URL as string;
-const ADMIN_UID:string = process.env.ADMIN_UID as string;
+
 let adminToken:string;
+let timer:number;
 const createAdminToken = async () => {
-  if (adminToken!=undefined) return;
-  await admin.auth().createCustomToken(ADMIN_UID).then((v)=> adminToken =v);
+  const now = Date.now();
+  if (adminToken==null||timer==null || now - timer > 1000*60*50) {
+    timer = Date.now();
+    await admin.auth().createCustomToken(ADMIN_UID, {cloudFunctions: true})
+        .then((v)=> adminToken =v);
+  } else {
+    return;
+  }
 };
 export const beforeCreate = functions.auth.user()
     .beforeCreate(async (user, context) => {
@@ -34,7 +45,8 @@ export const beforeCreate = functions.auth.user()
       const claimsResult = await axios
           .post<NotifyUserCreateResult>(APP_SERVER_URL, {
             query, variables,
-            headers: {"Content-Type": "application/json"},
+            headers: {"Content-Type": "application/json",
+              "Authorization": `Bearer ${adminToken}`},
           }).catch((e)=>{
             functions.logger.debug("error", e?.message);
             throw new functions.auth.HttpsError(
@@ -62,7 +74,8 @@ export const beforeSignIn = functions.auth.user()
       const claimsResult = await axios.post<UserRoles>(APP_SERVER_URL, {
         query,
         variables,
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+          "Authorization": `Bearer ${adminToken}`},
       }).catch((e)=>{
         functions.logger.debug("error", e?.message);
         throw new functions.auth.HttpsError(
